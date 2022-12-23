e6a22a2259b0022a44b4b8bc1a9471359584bd9c
HuyLM <huylm@ftech.ai>
2022-12-23 09:56:04 +0700

    info-b

--- Assets/raw_fb.cs
+++ Assets/raw_fb.cs
@@ -1,162 +1,397 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine.Serialization;
-
-namespace UnityEngine.UI.Test
-{
-    /// <summary>
-    /// Displays a Texture2D for the UI System.
-    /// </summary>
-    /// <remarks>
-    /// If you don't have or don't wish to create an atlas, you can simply use this script to draw a texture.
-    /// Keep in mind though that this will create an extra draw call with each RawImage present, so it's
-    /// best to use it only for backgrounds or temporary visible graphics.
-    /// </remarks>
-
-    [RequireComponent(typeof(CanvasRenderer))]
-    [AddComponentMenu("UI/Raw Image", 12)]
-    public class RawImage : MaskableGraphic
-    {
-        [FormerlySerializedAs("m_Tex")]
-        [SerializeField] Texture m_Texture;
-        [SerializeField] Rect m_UVRect = new Rect(0f, 0f, 1f, 1f);
-
-        protected RawImage()
-        {
-            useLegacyMeshGeneration = false;
-        }
-
-        /// <summary>
-        /// Returns the texture used to draw this Graphic.
-        /// </summary>
-        public override Texture mainTexture
-        {
-            get
-            {
-                if (m_Texture == null)
-                {
-                    if (material != null && material.mainTexture != null)
-                    {
-                        return material.mainTexture;
-                    }
-                    return s_WhiteTexture;
-                }
-
-                return m_Texture;
-            }
-        }
-
-        /// <summary>
-        /// The RawImage's texture to be used.
-        /// </summary>
-        /// <remarks>
-        /// Use this to alter or return the Texture the RawImage displays. The Raw Image can display any Texture whereas an Image component can only show a Sprite Texture.
-        /// Note : Keep in mind that using a RawImage creates an extra draw call with each RawImage present, so it's best to use it only for backgrounds or temporary visible graphics.Note: Keep in mind that using a RawImage creates an extra draw call with each RawImage present, so it's best to use it only for backgrounds or temporary visible graphics.
-        /// </remarks>
-        /// <example>
-        /// <code>
-        /// //Create a new RawImage by going to Create>UI>Raw Image in the hierarchy.
-        /// //Attach this script to the RawImage GameObject.
-        ///
-        /// using UnityEngine;
-        /// using UnityEngine.UI;
-        ///
-        /// public class RawImageTexture : MonoBehaviour
-        /// {
-        ///     RawImage m_RawImage;
-        ///     //Select a Texture in the Inspector to change to
-        ///     public Texture m_Texture;
-        ///
-        ///     void Start()
-        ///     {
-        ///         //Fetch the RawImage component from the GameObject
-        ///         m_RawImage = GetComponent<RawImage>();
-        ///         //Change the Texture to be the one you define in the Inspector
-        ///         m_RawImage.texture = m_Texture;
-        ///     }
-        /// }
-        /// </code>
-        /// </example>
-        public Texture texture
-        {
-            get
-            {
-                return m_Texture;
-            }
-            set
-            {
-                if (m_Texture == value)
-                    return;
-
-                m_Texture = value;
-                SetVerticesDirty();
-                SetMaterialDirty();
-            }
-        }
-
-        /// <summary>
-        /// UV rectangle used by the texture.
-        /// </summary>
-        public Rect uvRect
-        {
-            get
-            {
-                return m_UVRect;
-            }
-            set
-            {
-                if (m_UVRect == value)
-                    return;
-                m_UVRect = value;
-                SetVerticesDirty();
-            }
-        }
-
-        /// <summary>
-        /// Adjust the scale of the Graphic to make it pixel-perfect.
-        /// </summary>
-        /// <remarks>
-        /// This means setting the RawImage's RectTransform.sizeDelta  to be equal to the Texture dimensions.
-        /// </remarks>
-        public override void SetNativeSize()
-        {
-            Texture tex = mainTexture;
-            if (tex != null)
-            {
-                int w = Mathf.RoundToInt(tex.width * uvRect.width);
-                int h = Mathf.RoundToInt(tex.height * uvRect.height);
-                rectTransform.anchorMax = rectTransform.anchorMin;
-                rectTransform.sizeDelta = new Vector2(w, h);
-            }
-        }
-
-        protected override void OnPopulateMesh(VertexHelper vh)
-        {
-            Texture tex = mainTexture;
-            vh.Clear();
-            if (tex != null)
-            {
-                var r = GetPixelAdjustedRect();
-                var v = new Vector4(r.x, r.y, r.x + r.width, r.y + r.height);
-                var scaleX = tex.width * tex.texelSize.x;
-                var scaleY = tex.height * tex.texelSize.y;
-                {
-                    var color32 = color;
-                    vh.AddVert(new Vector3(v.x, v.y), color32, new Vector2(m_UVRect.xMin * scaleX, m_UVRect.yMin * scaleY));
-                    vh.AddVert(new Vector3(v.x, v.w), color32, new Vector2(m_UVRect.xMin * scaleX, m_UVRect.yMax * scaleY));
-                    vh.AddVert(new Vector3(v.z, v.w), color32, new Vector2(m_UVRect.xMax * scaleX, m_UVRect.yMax * scaleY));
-                    vh.AddVert(new Vector3(v.z, v.y), color32, new Vector2(m_UVRect.xMax * scaleX, m_UVRect.yMin * scaleY));
-
-                    vh.AddTriangle(0, 1, 2);
-                    vh.AddTriangle(2, 3, 0);
-                }
-            }
-        }
-
-        protected override void OnDidApplyAnimationProperties()
-        {
-            SetMaterialDirty();
-            SetVerticesDirty();
-        }
-    }
-}
+<? xml version = "1.0" encoding = "UTF-8" ?>
+   < !DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
+	< plist version = "1.0" >
+	 < dict >
+	 
+		 < key > CADisableMinimumFrameDuration </ key >
+	 
+		 < false />
+	 
+		 < key > CADisableMinimumFrameDurationOnPhone </ key >
+	 
+		 < false />
+	 
+		 < key > CFBundleAllowMixedLocalizations </ key >
+	 
+		 < true />
+	 
+		 < key > CFBundleDevelopmentRegion </ key >
+	 
+		 < string > en </ string >
+	 
+		 < key > CFBundleDisplayName </ key >
+	 
+		 < string > Racing Legends </ string >
+		
+			< key > CFBundleExecutable </ key >
+		
+			< string >${ EXECUTABLE_NAME}</ string >
+		   
+			   < key > CFBundleIdentifier </ key >
+		   
+			   < string >${ PRODUCT_BUNDLE_IDENTIFIER}</ string >
+			  
+				  < key > CFBundleInfoDictionaryVersion </ key >
+			  
+				  < string > 6.0 </ string >
+			  
+				  < key > CFBundleName </ key >
+			  
+				  < string >${ PRODUCT_NAME}</ string >
+				 
+					 < key > CFBundlePackageType </ key >
+				 
+					 < string > APPL </ string >
+				 
+					 < key > CFBundleShortVersionString </ key >
+				 
+					 < string >$(MARKETING_VERSION) </ string >
+				   
+					   < key > CFBundleURLTypes </ key >
+				   
+					   < array >
+				   
+						   < dict >
+				   
+							   < key > CFBundleURLName </ key >
+				   
+							   < string ></ string >
+				   
+							   < key > CFBundleURLSchemes </ key >
+				   
+							   < array >
+				   
+								   < string > racingdeeplink </ string >
+				   
+								   < string > ai.ftech.id.racing - dev </ string >
+				   
+								   < string > ai.ftech.id.racing </ string >
+				   
+							   </ array >
+				   
+						   </ dict >
+				   
+						   < dict >
+				   
+							   < key > CFBundleTypeRole </ key >
+				   
+							   < string > Editor </ string >
+				   
+							   < key > CFBundleURLName </ key >
+				   
+							   < string > google </ string >
+				   
+							   < key > CFBundleURLSchemes </ key >
+				   
+							   < array >
+				   
+								   < string > com.googleusercontent.apps.701600083154 - qn8i9locmcobk9qlg0c3r18ro1peu83a </ string >
+					  
+								  </ array >
+					  
+							  </ dict >
+					  
+							  < dict >
+					  
+								  < key > CFBundleTypeRole </ key >
+					  
+								  < string > Editor </ string >
+					  
+								  < key > CFBundleURLName </ key >
+					  
+								  < string > com.racing.mobile </ string >
+					  
+								  < key > CFBundleURLSchemes </ key >
+					  
+								  < array >
+					  
+									  < string > com.racing.mobile </ string >
+					  
+								  </ array >
+					  
+							  </ dict >
+					  
+							  < dict >
+					  
+								  < key > CFBundleURLName </ key >
+					  
+								  < string > facebook - unity - sdk </ string >
+					  
+								  < key > CFBundleURLSchemes </ key >
+					  
+								  < array >
+					  
+									  < string > fb667594578332522 </ string >
+					  
+								  </ array >
+					  
+							  </ dict >
+					  
+						  </ array >
+					  
+						  < key > CFBundleVersion </ key >
+					  
+						  < string >$(CURRENT_PROJECT_VERSION) </ string >
+						
+							< key > FacebookAdvertiserIDCollectionEnabled </ key >
+						
+							< true />
+						
+							< key > FacebookAppID </ key >
+						
+							< string > 667594578332522 </ string >
+						
+							< key > FacebookAutoLogAppEventsEnabled </ key >
+						
+							< true />
+						
+							< key > FacebookClientToken </ key >
+						
+							< string > d99fa389d3fb7a944a4153d20bb83a0b </ string >
+						
+							< key > GADApplicationIdentifier </ key >
+						
+							< string > ca - app - pub - 1430994929352486~5569862899 </ string >
+								 
+									 < key > LSApplicationQueriesSchemes </ key >
+								 
+									 < array >
+								 
+										 < string > fbapi </ string >
+								 
+										 < string > fb - messenger - api </ string >
+								 
+										 < string > fbauth2 </ string >
+								 
+										 < string > fbshareextension </ string >
+								 
+									 </ array >
+								 
+									 < key > LSRequiresIPhoneOS </ key >
+								 
+									 < true />
+								 
+									 < key > NSAppTransportSecurity </ key >
+								 
+									 < dict >
+								 
+										 < key > NSAllowsArbitraryLoads </ key >
+								 
+										 < true />
+								 
+										 < key > NSAllowsArbitraryLoadsInWebContent </ key >
+								 
+										 < true />
+								 
+									 </ dict >
+								 
+									 < key > NSPhotoLibraryAddUsageDescription </ key >
+								 
+									 < string > The app requires access to Photos to save media to it, which help you share game screen on social</string>
+	<key>NSPhotoLibraryUsageDescription</key>
+	<string>The app requires access to Photos, which help you share game screen on social</string>
+	<key>NSUserTrackingUsageDescription</key>
+	<string>Allowing app tracking will improve the customization of ads</string>
+	<key>SKAdNetworkItems</key>
+	<array>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>cstr6suwn9.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>4fzdc2evr5.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>2fnua5tdw4.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>ydx93a7ass.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>5a6flpkh64.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>p78axxw29g.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>v72qych5uu.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>c6k4g5qg8m.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>s39g8k73mm.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>3qy4746246.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>3sh42y64q3.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>f38h382jlk.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>hs6bdukanm.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>prcb7njmu6.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>v4nxqhlyqp.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>wzmmz9fp6w.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>yclnxrl5pm.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>t38b2kh725.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>7ug5zh24hu.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>9rd848q2bz.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>n6fk4nfna4.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>kbd757ywx3.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>9t245vhmpl.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>4468km3ulz.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>2u9pt9hc89.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>8s468mfl3y.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>av6w8kgt66.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>klf5c3l5u5.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>ppxm28t8ap.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>424m5254lk.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>uw77j35x4d.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>578prtvx9j.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>4dzt52r2t5.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>e5fvkxwrpn.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>8c4e2ghe7u.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>zq492l623r.skadnetwork</string>
+		</dict>
+		<dict>
+			<key>SKAdNetworkIdentifier</key>
+			<string>3qcr597p9d.skadnetwork</string>
+		</dict>
+	</array>
+	<key>UILaunchStoryboardName</key>
+	<string>LaunchScreen-iPhone</string>
+	<key>UILaunchStoryboardName~ipad</key>
+	<string>LaunchScreen-iPad</string>
+	<key>UILaunchStoryboardName~iphone</key>
+	<string>LaunchScreen-iPhone</string>
+	<key>UILaunchStoryboardName~ipod</key>
+	<string>LaunchScreen-iPhone</string>
+	<key>UIPrerenderedIcon</key>
+	<false/>
+	<key>UIRequiredDeviceCapabilities</key>
+	<array>
+		<string>arm64</string>
+		<string>metal</string>
+	</array>
+	<key>UIRequiresFullScreen</key>
+	<true/>
+	<key>UIRequiresPersistentWiFi</key>
+	<false/>
+	<key>UIStatusBarHidden</key>
+	<true/>
+	<key>UIStatusBarStyle</key>
+	<string>UIStatusBarStyleDefault</string>
+	<key>UISupportedInterfaceOrientations</key>
+	<array>
+		<string>UIInterfaceOrientationLandscapeRight</string>
+		<string>UIInterfaceOrientationLandscapeLeft</string>
+	</array>
+	<key>UIViewControllerBasedStatusBarAppearance</key>
+	<false/>
+	<key>UnityAddRemoteNotificationCapability</key>
+	<false/>
+	<key>UnityCloudProjectID</key>
+	<string></string>
+	<key>UnityCrashSubmissionURL</key>
+	<string></string>
+	<key>UnityNotificationDefaultAuthorizationOptions</key>
+	<integer>7</integer>
+	<key>UnityNotificationRequestAuthorizationForRemoteNotificationsOnAppLaunch</key>
+	<false/>
+	<key>UnityNotificationRequestAuthorizationOnAppLaunch</key>
+	<true/>
+	<key>UnityRemoteNotificationForegroundPresentationOptions</key>
+	<integer>-1</integer>
+	<key>UnityUseAPSReleaseEnvironment</key>
+	<false/>
+	<key>UnityUseLocationNotificationTrigger</key>
+	<false/>
+	<key>Unity_LoadingActivityIndicatorStyle</key>
+	<integer>-1</integer>
+</dict>
+</plist>
